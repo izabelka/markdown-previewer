a940334454585aadea7807be4ffc2b96e57e8e43
izabelka <i.konstanciak@gmail.com>
2017-02-06 19:59:26 +0100

    Add input field and make displaying instantly what is being typed in this input

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 var marked = require('marked');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.getInput = this.getInput.bind(this);
+  }
+  getInput(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
   render() {
     return (
       <div className="App">
+        <input type="text" value={this.state.input} onChange={this.getInput}/>
+        <div>{marked(this.state.input)}</div>
       </div>
     );
   }
